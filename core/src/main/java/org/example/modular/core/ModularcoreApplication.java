27049919700fe0016d7eb5d0c50dc5c9b73ce6fd
akomii <alexkombie@gmail.com>
2026-04-15 11:43:10 +0200

    init modular core

---
+++ core/src/main/java/org/example/modular/core/ModularcoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.modular.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModularcoreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ModularcoreApplication.class, args);
+  }
+
+}
